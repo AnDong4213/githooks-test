663f1852b1da23af479b5713a6380598c8f4516b
赵安东 <zhaoandong4213@sina.com>
2022-02-23 16:43:59 +0800

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-const [name, setName] = useState('小米')
+  const [name, setName] = useState("小米");
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit <code>src/App.tsx</code> and save to reload.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"
-        >Learn React</a>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <div><p>LOGO</p><img src={logo} className="App-logo" alt="logo" /><span>{name}</span><p>LOGO</p><p>LOGO</p></div>
+      <div>
+        <p>LOGO</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <span>{name}</span>
+        <p>LOGO</p>
+        <p>LOGO</p>
+      </div>
     </div>
   );
 }
