b0d4f3ea879a46275f079a621e037635b0f36fd0
赵安东 <zhaoandong4213@sina.com>
2022-02-24 15:11:59 +0800

    fix: 检测2

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState('小米');
-  var aa = 99;
+  const [name] = useState('小米');
+  // var aa = 99;
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+        <img alt='logo' className='App-logo' src={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className='App-link'
           href='https://reactjs.org'
-          target='_blank'
           rel='noopener noreferrer'
+          target='_blank'
         >
           Learn React
         </a>
       </header>
       <div>
         <p>LOGO</p>
-        <img src={logo} className='App-logo' alt='logo' />
-        <span>{name}</span> <p>LOGO</p> <p>LOGO</p> <p>LOGO</p>
+        <img alt='logo' className='App-logo' src={logo} />
+        <span>{name}</span>
         <p>LOGO</p>
         <p>LOGO</p>
       </div>
