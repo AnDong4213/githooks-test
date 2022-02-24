82d2f598dcae0fdd3b772c0c555b30e9931f9d68
赵安东 <zhaoandong4213@sina.com>
2022-02-24 14:44:16 +0800

    fix: 测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [name, setName] = useState("小米");
+  const [name, setName] = useState('小米');
+  var aa = 99;
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
       <div>
         <p>LOGO</p>
-        <img src={logo} className="App-logo" alt="logo" /> <span>{name}</span>
+        <img src={logo} className='App-logo' alt='logo' />
+        <span>{name}</span>
         <p>LOGO</p>
         <p>LOGO</p>
         <p>LOGO</p>
